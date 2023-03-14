6e864669434c3a51f2260056035b55ae3c194ab7
André Garcia <61523970+andregpereira@users.noreply.github.com>
2023-03-14 02:51:10 -0300

    Add entidade Pais

---
+++ src/main/java/com/github/andregpereira/resilientshop/userapi/entities/Pais.java
@@ -0,0 +1,42 @@
+package com.github.andregpereira.resilientshop.userapi.entities;
+
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@SequenceGenerator(name = "pais", sequenceName = "sq_paises", allocationSize = 1)
+@Table(name = "paises", uniqueConstraints = { @UniqueConstraint(name = "uc_codigo", columnNames = "codigo") })
+public class Pais {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "pais")
+	@Column(name = "id_pais")
+	private Long id;
+
+	@Column(name = "nome", length = 45, nullable = false)
+	private String nome;
+
+	@Column(name = "codigo", length = 3, nullable = false)
+	private String codigo;
+
+	@OneToMany(mappedBy = "pais", targetEntity = Endereco.class)
+	private List<Endereco> enderecos;
+
+}
