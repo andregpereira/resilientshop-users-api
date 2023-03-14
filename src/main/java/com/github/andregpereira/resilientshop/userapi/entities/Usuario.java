c79219d6170fb846c307f2421726dacbd2ebdf48
André Garcia <61523970+andregpereira@users.noreply.github.com>
2023-03-14 02:51:50 -0300

    Add atributo endereco

--- src/main/java/com/github/andregpereira/resilientshop/userapi/entities/Usuario.java
+++ src/main/java/com/github/andregpereira/resilientshop/userapi/entities/Usuario.java
@@ -2,11 +2,15 @@ package com.github.andregpereira.resilientshop.userapi.entities;
 
 import java.time.LocalDate;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.ForeignKey;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 import jakarta.persistence.UniqueConstraint;
@@ -15,16 +19,14 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Entity
-@Table(name = "usuarios", uniqueConstraints = {
-		@UniqueConstraint(name = "telefone_uc", columnNames = "telefone"),
-		@UniqueConstraint(name = "cpf_uc", columnNames = "cpf")
-		})
-@SequenceGenerator(name = "usuario", sequenceName = "usuario_sq", allocationSize = 1)
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
+@Entity
+@SequenceGenerator(name = "usuario", sequenceName = "sq_usuarios", allocationSize = 1)
+@Table(name = "usuarios", uniqueConstraints = { @UniqueConstraint(name = "uc_telefone", columnNames = "telefone"),
+		@UniqueConstraint(name = "uc_cpf", columnNames = "cpf") })
 public class Usuario {
 
 	@Id
@@ -32,22 +34,26 @@ public class Usuario {
 	@Column(name = "id_usuario")
 	private Long id;
 
-	@Column(name = "nome", length = 35, nullable = false)
+	@Column(name = "nome", length = 45, nullable = false)
 	private String nome;
 
-	@Column(name = "sobrenome", length = 60, nullable = false)
+	@Column(name = "sobrenome", length = 45, nullable = false)
 	private String sobrenome;
-
-	@Column(name = "telefone", length = 20, nullable = false)
-	private String telefone;
 
 	@Column(name = "cpf", length = 11, nullable = false)
 	private String cpf;
+
+	@Column(name = "telefone", length = 20, nullable = false)
+	private String telefone;
 
 	@Column(name = "data_criacao", nullable = false, updatable = false)
 	private LocalDate dataCriacao;
 
 	@Column(name = "data_modificacao")
 	private LocalDate dataModificacao;
+
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "id_endereco", foreignKey = @ForeignKey(name = "fk_id_endereco"))
+	private Endereco endereco;
 
 }
