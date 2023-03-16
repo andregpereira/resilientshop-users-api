40a53681aa93906d2bce954fab685bbabd0859c3
André Garcia <61523970+andregpereira@users.noreply.github.com>
2023-03-15 22:45:07 -0300

    Add consultas por nome e código

--- src/main/java/com/github/andregpereira/resilientshop/userapi/repositories/PaisRepository.java
+++ src/main/java/com/github/andregpereira/resilientshop/userapi/repositories/PaisRepository.java
@@ -1,5 +1,7 @@
 package com.github.andregpereira.resilientshop.userapi.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,9 @@ import com.github.andregpereira.resilientshop.userapi.entities.Pais;
 
 @Repository
 public interface PaisRepository extends JpaRepository<Pais, Long> {
+
+	Optional<Pais> findByNome(String nome);
+
+	Optional<Pais> findByCodigo(String codigo);
 
 }
