944c61a03fa7fe2f438027f19671447ae496f81c
André Garcia <61523970+andregpereira@users.noreply.github.com>
2023-03-17 12:53:41 -0300

    Refatorar DTOs

--- src/main/java/com/github/andregpereira/resilientshop/userapi/dtos/pais/PaisRegistroDto.java
+++ src/main/java/com/github/andregpereira/resilientshop/userapi/dtos/pais/PaisRegistroDto.java
@@ -6,6 +6,6 @@ import jakarta.validation.constraints.Size;
 
 public record PaisRegistroDto(
 		@NotBlank(message = "Insira o nome do país.") @Size(message = "O nome do país deve ter no máximo 45 caracteres.", max = 45) String nome,
-		@NotBlank(message = "Insira o código do país.") @Size(max = 45, message = "O código do país deve ter entre 2 e 3 caracteres") @Pattern(message = "Formato inválido. Formato: +xxx", regexp = "[+]\\d{3}") String codigo) {
+		@NotBlank(message = "Insira o código do país.") @Size(max = 4, message = "O código do país deve ter no máximo 4 caracteres") @Pattern(message = "Formato inválido. Formato: +xxx", regexp = "[+]\\d{3}") String codigo) {
 
 }
--- src/main/java/com/github/andregpereira/resilientshop/userapi/dtos/usuario/UsuarioRegistroDto.java
+++ src/main/java/com/github/andregpereira/resilientshop/userapi/dtos/usuario/UsuarioRegistroDto.java
@@ -15,9 +15,7 @@ import lombok.Builder;
 public record UsuarioRegistroDto(
 		@NotBlank(message = "Insira o nome.") @Size(message = "O nome deve ter entre 2 e 255 caracteres", min = 2, max = 255) String nome,
 		@NotBlank(message = "Insira o sobrenome.") @Size(message = "O sobrenome deve ter entre 2 e 255 caracteres", min = 2, max = 255) String sobrenome,
-		@NotBlank(message = "Insira o CPF.") @CPF(message = "Formato de CPF inválido. Formatos aceitos: xxx.xxx.xxx-xx, xxxxxxxxx-xx ou xxxxxxxxxxx")
-//		@Pattern(message = "Formato de CPF inválido. Formatos aceitos: xxx.xxx.xxx-xx, xxxxxxxxx-xx ou xxxxxxxxxxx", regexp = "^(\\d{3}[.]\\d{3}[.]\\d{3}[-]\\d{2})|(\\d{9}[-]\\d{2})|(\\d{11})$")
-		String cpf,
+		@NotBlank(message = "Insira o CPF.") @CPF(message = "CPF inválido ou inexistente. Formatos aceitos: xxx.xxx.xxx-xx, xxxxxxxxx-xx ou xxxxxxxxxxx") String cpf,
 		@NotBlank(message = "Insira o telefone.") @Size(message = "O telefone deve ter no máximo 20 caracteres.", max = 20) @Pattern(message = "Formato do telefone inválido. Por favor, informe um telefone no seguinte formato +xxx (xx) xxxxx-xxxx", regexp = "^[+]\\d{2,3} [(]\\d{2}[)] \\d{4,5}[-]\\d{4}$") String telefone,
 		@NotNull(message = "Insira o endereço.") @Valid EnderecoRegistroDto endereco) {
 }
