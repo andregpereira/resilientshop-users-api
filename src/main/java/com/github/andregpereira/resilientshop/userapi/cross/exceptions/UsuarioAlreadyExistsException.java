cb501ad5147fec35e4ca9f3af0b40e9d383c049a
André Garcia <61523970+andregpereira@users.noreply.github.com>
2023-04-28 19:53:16 -0300

    Refatorar exception

--- src/main/java/com/github/andregpereira/resilientshop/userapi/cross/exceptions/UsuarioAlreadyExistsException.java
+++ src/main/java/com/github/andregpereira/resilientshop/userapi/cross/exceptions/UsuarioAlreadyExistsException.java
@@ -1,9 +1,13 @@
 package com.github.andregpereira.resilientshop.userapi.cross.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.CONFLICT)
 public class UsuarioAlreadyExistsException extends RuntimeException {
 
-    public UsuarioAlreadyExistsException(String message) {
-        super(message);
+    public UsuarioAlreadyExistsException() {
+        super("Opa! Já existe um usuário cadastrado com esse CPF");
     }
 
 }
