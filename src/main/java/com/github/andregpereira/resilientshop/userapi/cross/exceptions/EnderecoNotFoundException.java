c7179f62e890300db4a1df08953fd5f74951cd87
André Garcia <61523970+andregpereira@users.noreply.github.com>
2023-05-04 14:35:08 -0300

    Criar exceção de endereço não encontrado

---
+++ src/main/java/com/github/andregpereira/resilientshop/userapi/cross/exceptions/EnderecoNotFoundException.java
@@ -0,0 +1,31 @@
+package com.github.andregpereira.resilientshop.userapi.cross.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.text.MessageFormat;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class EnderecoNotFoundException extends RuntimeException {
+
+    public EnderecoNotFoundException() {
+        super("Opa! Não há endereços cadastrados");
+    }
+
+    public EnderecoNotFoundException(Long id) {
+        super(MessageFormat.format("Não foi possível encontrar um endereço com o id {0}. Verifique e tente novamente",
+                id));
+    }
+
+    public EnderecoNotFoundException(String apelido) {
+        super(MessageFormat.format(
+                "Não foi possível encontrar um endereço com o apelido {0}. Verifique e tente novamente", apelido));
+    }
+
+    public EnderecoNotFoundException(String apelido, Long idUsuario) {
+        super(MessageFormat.format(
+                "Não foi possível encontrar um endereço com o apelido {0}. O usuário com id {1} está inativo", apelido,
+                idUsuario));
+    }
+
+}
