5d6ff2ac0391d0924aff391c47d5d88ce0beb69c
André Garcia <61523970+andregpereira@users.noreply.github.com>
2023-05-04 15:07:36 -0300

    Refatorar código

--- src/main/java/com/github/andregpereira/resilientshop/userapi/infra/entities/Usuario.java
+++ src/main/java/com/github/andregpereira/resilientshop/userapi/infra/entities/Usuario.java
@@ -46,7 +46,8 @@ public class Usuario {
     @Column(nullable = false)
     private boolean ativo;
 
-    @OneToMany(mappedBy = "usuario")
+    @OneToMany(mappedBy = "usuario",
+            cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.REFRESH, CascadeType.REMOVE})
     private List<Endereco> enderecos;
 
     @Override
