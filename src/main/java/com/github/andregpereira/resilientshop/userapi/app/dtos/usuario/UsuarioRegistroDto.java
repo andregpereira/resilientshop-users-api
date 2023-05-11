3fb9b1ad17c2466cc6e2590ff0b5134b5304f381
André Garcia <61523970+andregpereira@users.noreply.github.com>
2023-05-11 16:11:00 -0300

    Refatorar código

--- src/main/java/com/github/andregpereira/resilientshop/userapi/app/dtos/usuario/UsuarioRegistroDto.java
+++ src/main/java/com/github/andregpereira/resilientshop/userapi/app/dtos/usuario/UsuarioRegistroDto.java
@@ -14,7 +14,7 @@ public record UsuarioRegistroDto(@NotBlank(message = "Insira o nome") @Size(
         message = "O nome deve ter entre 2 e 255 caracteres", min = 2, max = 255) String nome,
         @NotBlank(message = "Insira o sobrenome") @Size(message = "O sobrenome deve ter entre 2 e 255 caracteres",
                 min = 2, max = 255) String sobrenome,
-        @NotBlank(message = "Insira o CPF") @Size(message = "O CPF deve ter 11 números", min = 11) @CPF(
+        @NotBlank(message = "Insira o CPF") @CPF(
                 message = "CPF inválido ou inexistente. Formatos aceitos: xxx.xxx.xxx-xx, xxxxxxxxx-xx ou xxxxxxxxxxx") String cpf,
         @Size(message = "O telefone deve ter no máximo 20 caracteres", max = 20) @Pattern(
                 message = "Formato do telefone inválido. Por favor, informe um telefone no seguinte formato +xxx (xx) xxxxx-xxxx",
