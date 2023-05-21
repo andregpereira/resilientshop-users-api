aeff169738d222e311583087ce95cb83a39c5983
André Garcia <61523970+andregpereira@users.noreply.github.com>
2023-05-21 18:31:12 -0300

    Documentar código do UsuarioController com Javadoc

--- src/main/java/com/github/andregpereira/resilientshop/userapi/app/controllers/UsuarioController.java
+++ src/main/java/com/github/andregpereira/resilientshop/userapi/app/controllers/UsuarioController.java
@@ -24,6 +24,9 @@ import org.springframework.web.util.UriComponentsBuilder;
 import java.net.URI;
 import java.util.Optional;
 
+/**
+ * Controller de usuários da API de Usuários
+ */
 @RequiredArgsConstructor
 @Slf4j
 @Validated
@@ -31,21 +34,42 @@ import java.util.Optional;
 @RequestMapping("/usuarios")
 public class UsuarioController {
 
+    /**
+     * Injeção da dependência {@link UsuarioManutencaoService} para serviços de manutenção.
+     */
     private final UsuarioManutencaoService usuarioManutencaoService;
+
+    /**
+     * Injeção da dependência {@link UsuarioConsultaService} para serviços de consulta.
+     */
     private final UsuarioConsultaService usuarioConsultaService;
 
-    // Registrar usuário
+    /**
+     * Cadastra um {@linkplain UsuarioRegistroDto usuário}.
+     * Retorna um {@linkplain UsuarioDetalhesDto usuário detalhado}.
+     *
+     * @param dto o usuário a ser cadastrado.
+     *
+     * @return o usuário criado.
+     */
     @PostMapping
-    public ResponseEntity<UsuarioDetalhesDto> registrar(@RequestBody @Valid UsuarioRegistroDto dto,
-            UriComponentsBuilder uriBuilder) {
+    public ResponseEntity<UsuarioDetalhesDto> registrar(@RequestBody @Valid UsuarioRegistroDto dto) {
         log.info("Criando usuário...");
         UsuarioDetalhesDto usuario = usuarioManutencaoService.registrar(dto);
-        URI uri = uriBuilder.path("/usuarios/{id}").buildAndExpand(usuario.id()).toUri();
+        URI uri = UriComponentsBuilder.fromPath("/usuarios/{id}").buildAndExpand(usuario.id()).toUri();
         log.info("Usuário criado com sucesso");
         return ResponseEntity.created(uri).body(usuario);
     }
 
-    // Atualizar usuário por id
+    /**
+     * Atualiza um {@linkplain UsuarioAtualizacaoDto usuário} por {@code id}.
+     * Retorna um {@linkplain UsuarioDetalhesDto usuário detalhado}.
+     *
+     * @param id  o id do usuário a ser atualizado.
+     * @param dto os dados do usuário a serem atualizados.
+     *
+     * @return o usuário atualizado.
+     */
     @PutMapping("/{id}")
     public ResponseEntity<UsuarioDetalhesDto> atualizar(@PathVariable Long id,
             @RequestBody @Valid UsuarioAtualizacaoDto dto) {
@@ -53,21 +77,42 @@ public class UsuarioController {
         return ResponseEntity.ok(usuarioManutencaoService.atualizar(id, dto));
     }
 
-    // Remoção lógica de usuário por id
+    /**
+     * Remoção lógica de um usuário por {@code id}.
+     * Retorna uma mensagem de confirmação de desativação.
+     *
+     * @param id o id do usuário a ser desativado.
+     *
+     * @return uma mensagem de confirmação de desativação.
+     */
     @DeleteMapping("/{id}")
     public ResponseEntity<String> desativar(@PathVariable Long id) {
         log.info("Desativando usuário...");
         return ResponseEntity.ok(usuarioManutencaoService.desativar(id));
     }
 
-    // Reativar por id
+    /**
+     * Reativa um usuário por {@code id}.
+     * Retorna uma mensagem de confirmação de reativação.
+     *
+     * @param id o id do usuário a ser reaativado.
+     *
+     * @return uma mensagem de confirmação de reativação.
+     */
     @PatchMapping("/reativar/{id}")
     public ResponseEntity<String> reativar(@PathVariable Long id) {
         log.info("Reativando usuário...");
         return ResponseEntity.ok(usuarioManutencaoService.reativar(id));
     }
 
-    // Listar usuários
+    /**
+     * Lista todos os usuários cadastrados.
+     * Retorna uma {@linkplain Page sublista} de {@linkplain UsuarioDto usuários}.
+     *
+     * @param pageable o pageable padrão.
+     *
+     * @return uma sublista de uma lista com todos os usuários cadastrados.
+     */
     @GetMapping
     public ResponseEntity<Page<UsuarioDto>> listar(
             @Parameter(hidden = true) @PageableDefault(sort = "id", direction = Direction.ASC, page = 0, size = 10)
@@ -76,14 +121,28 @@ public class UsuarioController {
         return ResponseEntity.ok(usuarioConsultaService.listar(pageable));
     }
 
-    // Pesquisar por id
+    /**
+     * Pesquisa um usuário por {@code id}.
+     * Retorna um {@linkplain UsuarioDetalhesDto usuário detalhado}.
+     *
+     * @param id o id do usuário a ser consultado.
+     *
+     * @return um usuário encontrado pelo {@code id}.
+     */
     @GetMapping("/{id}")
     public ResponseEntity<UsuarioDetalhesDto> consultarPorId(@PathVariable Long id) {
         log.info("Consultando usuário por id {}...", id);
         return ResponseEntity.ok(usuarioConsultaService.consultarPorId(id));
     }
 
-    // Pesquisar por CPF
+    /**
+     * Pesquisa um usuário por {@code cpf}.
+     * Retorna um {@linkplain UsuarioDetalhesDto usuário detalhado}.
+     *
+     * @param cpf o CPF do usuário a ser consultado.
+     *
+     * @return um usuário encontrado pelo {@code cpf}.
+     */
     @GetMapping("/cpf")
     public ResponseEntity<UsuarioDetalhesDto> consultarPorCpf(@RequestParam
     @Pattern(message = "CPF inválido. Formatos aceitos: xxx.xxx.xxx-xx, xxxxxxxxx-xx ou xxxxxxxxxxx",
@@ -92,7 +151,15 @@ public class UsuarioController {
         return ResponseEntity.ok(usuarioConsultaService.consultarPorCpf(cpf.replace(".", "").replace("-", "")));
     }
 
-    // Pesquisar por nome
+    /**
+     * Pesquisa usuários por {@code nome} e {@code sobrenome}.
+     * Retorna uma {@linkplain Page sublista} de {@linkplain UsuarioDto usuários}.
+     *
+     * @param nome      o nome dos usuários a serem consultados.
+     * @param sobrenome (opcional) o sobrenome dos usuários a serem consultados.
+     *
+     * @return uma sublista de uma lista com todos os usuários encontrados pelo {@code nome} e {@code sobrenome}.
+     */
     @GetMapping("/nome")
     public ResponseEntity<Page<UsuarioDto>> consultarPorNome(
             @RequestParam @Size(message = "O nome deve ter pelo menos 2 caracteres", min = 2) String nome,
