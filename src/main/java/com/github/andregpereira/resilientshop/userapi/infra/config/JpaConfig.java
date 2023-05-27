52ca6171d8d27b2126b911d1e0a05b2b7814633b
André Garcia <61523970+andregpereira@users.noreply.github.com>
2023-05-27 19:02:14 -0300

    Criar classe de configuração do JPA e habilitar auditoria

---
+++ src/main/java/com/github/andregpereira/resilientshop/userapi/infra/config/JpaConfig.java
@@ -0,0 +1,10 @@
+package com.github.andregpereira.resilientshop.userapi.infra.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+
+}
