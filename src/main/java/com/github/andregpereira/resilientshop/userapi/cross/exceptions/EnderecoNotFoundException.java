35fe0ac3fadb56fb52d3374913e41fac35e1e1fa
André Garcia <61523970+andregpereira@users.noreply.github.com>
2023-06-01 16:07:25 -0300

    Refatorar código

--- src/main/java/com/github/andregpereira/resilientshop/userapi/cross/exceptions/EnderecoNotFoundException.java
+++ src/main/java/com/github/andregpereira/resilientshop/userapi/cross/exceptions/EnderecoNotFoundException.java
@@ -8,20 +8,32 @@ import java.text.MessageFormat;
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class EnderecoNotFoundException extends RuntimeException {
 
-    public EnderecoNotFoundException(Long id) {
-        super(MessageFormat.format("Não foi possível encontrar um endereço com o id {0}. Verifique e tente novamente",
-                id));
+    public EnderecoNotFoundException(String tipo, Long id) {
+        super(definirTipo(tipo, id));
+    }
+
+    public EnderecoNotFoundException(Long id, Long idUsuario) {
+        super(MessageFormat.format("Opa! O usuário com id {0} não possui endereços cadastrados", idUsuario));
     }
 
     public EnderecoNotFoundException(String apelido) {
         super(MessageFormat.format(
                 "Não foi possível encontrar um endereço com o apelido {0}. Verifique e tente novamente", apelido));
     }
+//    public EnderecoNotFoundException(String apelido, Long idUsuario) {
+//        super(MessageFormat.format(
+//                "Não foi possível encontrar um endereço com o apelido {0}. O usuário com id {1} está inativo", apelido,
+//                idUsuario));
+//    }
 
-    public EnderecoNotFoundException(String apelido, Long idUsuario) {
-        super(MessageFormat.format(
-                "Não foi possível encontrar um endereço com o apelido {0}. O usuário com id {1} está inativo", apelido,
-                idUsuario));
+    private static String definirTipo(String tipo, Object atributo) {
+        if ("endereço".equals(tipo))
+            return MessageFormat.format(
+                    "Não foi possível encontrar um endereço com o id {0}. Verifique e tente novamente", atributo);
+        else if ("usuário".equals(tipo))
+            return MessageFormat.format("Opa! O usuário com id {0} não possui endereços cadastrados", atributo);
+        else
+            return MessageFormat.format("Não foi possível encontrar um endereço com o apelido {0}", tipo);
     }
 
 }
