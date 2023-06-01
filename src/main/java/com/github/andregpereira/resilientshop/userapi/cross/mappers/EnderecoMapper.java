5afd17bea9226a8b18977b0efdd081d81767638f
André Garcia <61523970+andregpereira@users.noreply.github.com>
2023-06-01 16:07:39 -0300

    Refatorar DTO

--- src/main/java/com/github/andregpereira/resilientshop/userapi/cross/mappers/EnderecoMapper.java
+++ src/main/java/com/github/andregpereira/resilientshop/userapi/cross/mappers/EnderecoMapper.java
@@ -1,6 +1,7 @@
 package com.github.andregpereira.resilientshop.userapi.cross.mappers;
 
 import com.github.andregpereira.resilientshop.userapi.app.dto.endereco.EnderecoDto;
+import com.github.andregpereira.resilientshop.userapi.app.dto.endereco.EnderecoRegistroUsuarioDto;
 import com.github.andregpereira.resilientshop.userapi.app.dto.endereco.EnderecoRegistroDto;
 import com.github.andregpereira.resilientshop.userapi.infra.entities.Endereco;
 import org.mapstruct.Mapper;
@@ -12,16 +13,18 @@ import java.util.List;
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING, unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface EnderecoMapper {
 
-    List<Endereco> listaEnderecoRegistroDtoToListaEnderecos(List<EnderecoRegistroDto> dtos);
+    Endereco toEndereco(EnderecoRegistroUsuarioDto dto);
+
+    Endereco toEndereco(EnderecoRegistroDto dto);
+
+    Endereco toEndereco(EnderecoDto dto);
+
+    EnderecoDto toEnderecoDto(Endereco endereco);
+
+    List<Endereco> listaEnderecoRegistroDtoToListaEnderecos(List<EnderecoRegistroUsuarioDto> dtos);
 
     List<Endereco> listaEnderecoDtoToListaEnderecos(List<EnderecoDto> dtos);
 
     List<EnderecoDto> toListaEnderecosDto(List<Endereco> enderecos);
-
-    Endereco enderecoRegistroDtoToEndereco(EnderecoRegistroDto dto);
-
-    Endereco enderecoDtoToEndereco(EnderecoDto dto);
-
-    EnderecoDto toEnderecoDto(Endereco endereco);
 
 }
